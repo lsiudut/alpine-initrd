16f3266b883913eb379ba700bfd206bf882ab08e
Lukasz Siudut <lsiudut@gmail.com>
2017-11-28 18:31:17 +0000

    ouch. ttyS1->ttyS0

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ sudo docker run -t -i --rm -v "$PWD/alpine/:/alpine/" lucks/alpine-builder
 
 # enable ttyS0 and add it as secure tty
 sed -i 's/#\(ttyS0.*\)/\1/' alpine/etc/inittab
-echo ttyS1 >> alpine/etc/securetty
+echo ttyS0 >> alpine/etc/securetty
 
 cd alpine/ && \
   sudo find . | sudo cpio -o -H newc | xz -C crc32 -z -9 --threads=0 -c - > ../alpine-initrd.xz
